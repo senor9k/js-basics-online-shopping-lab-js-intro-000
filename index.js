efb95ed03c55c2fc82c47992b0380df717ba502a
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-12 00:02:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (let i=0; i<cart.length; i++) {
+    if (item === cart.itemName) {
+      cart.splice(i,1)
+    }
+  }
+
 }
 
 function placeOrder(cardNumber) {
