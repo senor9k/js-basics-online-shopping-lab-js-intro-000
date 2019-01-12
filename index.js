0ddb6e8e7aca065a2641cd3fea242f9faefe9779
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-12 00:05:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (let i=0; i<cart.length; i++) {
-    if (item === cart.itemName) {
+    if (item === cart[i].itemName) {
       return cart.splice(i,1)
     }
   }
